e80ce48f00e8622f6c88410a6f90db823991a995
Huynguyencode835 <142970556+Huynguyencode835@users.noreply.github.com>
2026-07-12 13:55:11 +0700

    QF-91 Tạo model Nhà hàng, món ăn, user
    tạo model và dữ liệu cho nhà hàng và món ăn

--- KHMFoodie/app/__init__.py
+++ KHMFoodie/app/__init__.py
@@ -1,12 +1,36 @@
+import os
 from flask import Flask
 from app.config import config_map
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from authlib.integrations.flask_client import OAuth
+from dotenv import load_dotenv
+import cloudinary
+
+
+load_dotenv()
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+
+cloudinary.config(
+    cloud_name= os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET')
+)
 
 
 def create_app(config_name='dev'):
     app = Flask(__name__)
+
     app.config.from_object(config_map[config_name])
 
+    login_manager.init_app(app)
+    db.init_app(app)
+
     from app.routes import register_routes
+
     register_routes(app)
 
     return app
