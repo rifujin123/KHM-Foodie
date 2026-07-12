40d2f2e3bee1fbf80b30fe375136319890297994
Huynguyencode835 <142970556+Huynguyencode835@users.noreply.github.com>
2026-07-12 20:13:49 +0700

    fix/QF-49-loiketnoi

--- KHMFoodie/app/__init__.py
+++ KHMFoodie/app/__init__.py
@@ -5,12 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv
+from app.extensions import db
 import cloudinary
 
 
 load_dotenv()
 
-db = SQLAlchemy()
 login_manager = LoginManager()
 
 
