a52eedf8459cbc9fc0a9ea0a9e75e170c7898c42
PH31M <ducminh03102005@gmail.com>
2026-07-12 21:08:12 +0700

    QF - 80 Thiết kế giao diện v1

--- KHMFoodie/app/controllers/controller.py
+++ KHMFoodie/app/controllers/controller.py
@@ -3,3 +3,7 @@ from flask import render_template
 
 def index():
     return render_template('testCustomer.html', title='KHM Foodie', description='Welcome to KHM Foodie! Explore the best food in Cambodia.')
+
+
+def search_customer():
+    return render_template('searchCustomer.html', title='Search Customer', description = 'Search for customers in the KHM Foodie database.')
--- KHMFoodie/app/routes/test.py
+++ KHMFoodie/app/routes/test.py
@@ -1,6 +1,9 @@
 from app.controllers.controller import index
 from flask import Blueprint
 
+from app.controllers.controller import search_customer
+
 test_bp = Blueprint('test_bp', __name__)
 
 test_bp.add_url_rule('/', view_func=index, methods=['GET'])
+test_bp.add_url_rule('/search_customer', view_func=search_customer, methods=['GET'])
