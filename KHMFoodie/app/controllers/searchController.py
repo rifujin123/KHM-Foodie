c681d41bc1de4776a4b5196dc4e203e4e6251858
PH31M <ducminh03102005@gmail.com>
2026-07-18 18:43:26 +0700

    QF-51 Tim kiem theo mon an

--- KHMFoodie/app/controllers/searchController.py
+++ KHMFoodie/app/controllers/searchController.py
@@ -3,28 +3,6 @@ from flask import jsonify, request, render_template
 
 
 class SearchController:
-    @staticmethod
-    def search_restaurants():
-        keyword = request.args.get('q', '').strip()
-        restaurants = RestaurantsDao.search_restaurants(keyword)
-
-        data = []
-
-        for r in restaurants:
-            data.append({
-                "id": r.id,
-                "name": r.name,
-                "address": r.address,
-                "avatar": r.avatar,
-                "cover_image": r.cover_image,
-                "description": r.description,
-                "cuisine_type": r.cuisine_type.value if r.cuisine_type else None,
-                "opening_time": r.opening_time.strftime("%H:%M") if r.opening_time else None,
-                "closing_time": r.closing_time.strftime("%H:%M") if r.closing_time else None
-            })
-
-        return jsonify({"data": data, "keyword": keyword}), 200
-    
 
     @staticmethod
     def search_restaurants():
